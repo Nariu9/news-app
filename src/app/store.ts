c5e87d7fab8f880d61f25ac1309eed6684c36cf2
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-13 22:54:53 +0200

    feat: implement Redux store and useAppDispatch, useAppSelector hooks

---
+++ src/app/store.ts
@@ -0,0 +1,11 @@
+import {configureStore} from '@reduxjs/toolkit';
+import {newsReducer} from '../features/News/newsSlice';
+
+export const store = configureStore({
+    reducer: {
+        news: newsReducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
