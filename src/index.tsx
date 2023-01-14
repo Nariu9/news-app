ce7adbc94684ad022c9bec1879b085ce19438f9f
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-14 22:42:26 +0200

    refactor: remove React.StrictMode, add Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}><App /></Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
