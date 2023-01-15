ae80cf5f5ceb77853adf92675280fddb85dc4442
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-15 22:27:42 +0200

    feat: add router and theme to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {createTheme, CssBaseline, ThemeProvider} from '@mui/material';
+import {createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider} from 'react-router-dom';
+import {HomePage} from './pages/HomePage/HomePage';
+import {ArticlePage} from './pages/ArticlePage/ArticlePage';
+import {Root} from './common/components/Root/Root';
+import {PageNotFound} from './pages/PageNotFound/PageNotFound';
+import {ErrorSnackbar} from './common/components/ErrorSnackbar/ErrorSnackbar';
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path={'/'} element={<Root/>}>
+        <Route index element={<HomePage/>}/>
+        <Route path={'/article/:id'} element={<ArticlePage/>}/>
+        <Route path={'/404'} element={<PageNotFound/>}/>
+        <Route path={'*'} element={<Navigate to={'/404'}/>}/>
+    </Route>
+))
+
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Montserrat',
+        allVariants: {
+            color: '#363636'
+        }
+    }
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <ErrorSnackbar/>
+            <RouterProvider router={router}/>
+        </ThemeProvider>
+    );
 }
 
 export default App;
