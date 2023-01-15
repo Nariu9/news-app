af0ee46fc9b700ee071bd1a9ef8b070565cfd668
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-15 22:24:18 +0200

    feat: add loading and error handling

--- src/features/News/newsSlice.ts
+++ src/features/News/newsSlice.ts
@@ -1,22 +1,37 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {ArticleType, newsAPI} from '../../api/news-api';
+import {AxiosError} from 'axios';
 
 
-export const fetchArticles = createAsyncThunk('news/fetchArticles', async (arg, {rejectWithValue})=> {
+export const fetchArticles = createAsyncThunk<ArticleType[], undefined, { rejectValue: string }>('news/fetchArticles', async (arg, {
+    rejectWithValue,
+    dispatch
+}) => {
+    dispatch(changeRequestStatus({loading: true}))
     try {
         const res = await newsAPI.getArticles()
         return res.data
     } catch (e) {
-        return rejectWithValue(null)
+        const error = e as AxiosError
+        return rejectWithValue(error.message)
+    } finally {
+        dispatch(changeRequestStatus({loading: false}))
     }
 })
 
-export const fetchArticleById = createAsyncThunk('news/fetchArticleById', async (articleId: string, {rejectWithValue})=> {
+export const fetchArticleById = createAsyncThunk<ArticleType, string, { rejectValue: string }>('news/fetchArticleById', async (articleId, {
+    rejectWithValue,
+    dispatch
+}) => {
+    dispatch(changeRequestStatus({loading: true}))
     try {
         const res = await newsAPI.getArticle(articleId)
         return res.data
     } catch (e) {
-        return rejectWithValue(null)
+        const error = e as AxiosError
+        return rejectWithValue(error.message)
+    } finally {
+        dispatch(changeRequestStatus({loading: false}))
     }
 })
 
@@ -24,11 +39,19 @@ export const newsSlice = createSlice({
     name: 'news',
     initialState: {
         articles: [] as ArticleType[],
-        article: {} as ArticleType
+        article: {} as ArticleType,
+        error: null as null | string,
+        isLoading: false
     },
     reducers: {
-        clearArticleData:(state) => {
+        clearArticleData: (state) => {
             state.article = {} as ArticleType
+        },
+        resetError: (state) => {
+            state.error = null
+        },
+        changeRequestStatus: (state, action: PayloadAction<{ loading: boolean }>) => {
+            state.isLoading = action.payload.loading
         }
     },
     extraReducers: builder => {
@@ -36,12 +59,17 @@ export const newsSlice = createSlice({
             .addCase(fetchArticles.fulfilled, (state, action) => {
                 state.articles = action.payload
             })
+            .addCase(fetchArticles.rejected, (state, action) => {
+                if (action.payload) state.error = action.payload
+            })
             .addCase(fetchArticleById.fulfilled, (state, action) => {
                 state.article = action.payload
+            })
+            .addCase(fetchArticleById.rejected, (state, action) => {
+                if (action.payload) state.error = action.payload
             })
     }
 })
 
-
-export  const {clearArticleData} = newsSlice.actions
+export const {clearArticleData, resetError, changeRequestStatus} = newsSlice.actions
 export const newsReducer = newsSlice.reducer
