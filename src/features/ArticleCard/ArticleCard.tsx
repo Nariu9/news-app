180e4a53f92e8013624264427fc2197eb3db8c18
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-16 15:25:46 +0200

    refactor: update image component and Typography props in ArticleCard

--- src/features/ArticleCard/ArticleCard.tsx
+++ src/features/ArticleCard/ArticleCard.tsx
@@ -1,39 +1,34 @@
-import React, {FC} from 'react';
-import {ArticleType} from '../../api/news-api';
-import {formatDate} from '../../common/utils/formatDate';
-import {Card, CardContent, CardMedia, Typography} from '@mui/material';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
-import classes from './ArticleCard.module.scss';
-import {Link} from 'react-router-dom';
+import { type FC } from 'react'
+import { type ArticleType } from '../../api/news-api'
+import { formatDate } from '../../common/utils/formatDate'
+import { Card, CardContent, Typography } from '@mui/material'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined'
+import classes from './ArticleCard.module.scss'
+import { Link } from 'react-router-dom'
+import { Image } from 'mui-image'
 
 type ArticleCardType = {
-    article: ArticleType
+  article: ArticleType
 }
 
-export const ArticleCard: FC<ArticleCardType> = ({article}) => {
-    return (
-        <Card className={classes.card}>
-            <CardMedia
-                component="img"
-                alt="article picture"
-                height="200"
-                image={article.imageUrl}
-            />
-            <CardContent className={classes.cardContent}>
-                <Typography variant="subtitle2" component="time" className={classes.dateBlock}>
-                    <CalendarTodayOutlinedIcon className={classes.calendar}/> {formatDate(article.publishedAt)}
-                </Typography>
-                <Typography variant="h5" component="h3">
-                    {article.title}
-                </Typography>
-                <Typography variant="subtitle1" component="p" className={classes.description}>
-                    {article.summary.length > 100
-                        ? `${article.summary.substring(0, 100)}...`
-                        : article.summary}
-                </Typography>
-                <Link to={`article/${article.id}`} className={classes.link}>Read more <ArrowForwardIcon className={classes.arrow}/></Link>
-            </CardContent>
-        </Card>
-    )
-}
+export const ArticleCard: FC<ArticleCardType> = ({ article }) => (
+  <Card className={classes.card}>
+    <Image src={article.imageUrl} height={'200px'} />
+    <CardContent className={classes.cardContent}>
+      <Typography variant='subtitle2' component='time' className={classes.dateBlock}>
+        <CalendarTodayOutlinedIcon className={classes.calendar} /> {formatDate(article.publishedAt)}
+      </Typography>
+      <Typography variant='h5' component='h3' dangerouslySetInnerHTML={{ __html: article.title }} />
+      <Typography
+        variant='subtitle1'
+        component='p'
+        className={classes.description}
+        dangerouslySetInnerHTML={{ __html: article.summary }}
+      />
+      <Link to={`article/${article.id}`} className={classes.link}>
+        Read more <ArrowForwardIcon className={classes.arrow} />
+      </Link>
+    </CardContent>
+  </Card>
+)
