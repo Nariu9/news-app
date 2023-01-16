a3e8aa3ae95fdaa08ad8b8cdaf44154513f16eb1
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-01-16 23:27:25 +0200

    refactor: update PageNotFound structure and style

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,5 +1,20 @@
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import classes from './PageNotFound.module.scss'
+import { NavArrow } from '../../common/components/NavArrow/NavArrow'
+import React from 'react'
+
 export const PageNotFound = () => (
-  <div>
-    <h1>Page not found</h1>
-  </div>
+  <Container maxWidth={'lg'} sx={{ display: 'flex' }} className={classes.container}>
+    <Typography variant={'h4'} component={'h2'} gutterBottom>
+      Sorry, this page is not available
+    </Typography>
+    <Typography variant={'h5'} component={'p'} className={classes.subtitle}>
+      Please, go back and try something else
+      <Typography component={'span'} className={classes.emoji}>
+        👨‍🚀🚀
+      </Typography>
+    </Typography>
+    <NavArrow text='Back to homepage' to={'/'} goBack />
+  </Container>
 )
